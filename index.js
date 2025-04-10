7c8365a1210cf9a8710c650fb8e949a72ca7809b
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-04-10 21:25:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ import cors from "cors";
 import CatatanRoute from "./routes/CatatanRoute.js";
 
 const app = express();
-app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => res.render("index"));
 app.use(CatatanRoute);
 
 app.listen(5000, () => console.log("Server connected"));
