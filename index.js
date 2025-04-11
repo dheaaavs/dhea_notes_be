1daad79857b334833decef40c15dc4888efd453b
dheaaavs <162094908+dheaaavs@users.noreply.github.com>
2025-04-11 10:55:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ import CatatanRoute from "./routes/CatatanRoute.js";
 const app = express();
 app.set("view engine", "ejs");
 
-app.use(cors({
-  origin: 'https://e-13-450704.uc.r.appspot.com',  // hanya mengizinkan origin ini
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(CatatanRoute);
